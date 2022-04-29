3a161036c4d3c828ea1263adcf24cd84b6c32659
musa_dv <musa_dv@abv.bg>
2022-04-29 15:47:56 +0300

    book card style changes

--- book-store/src/components/Book/Book.js
+++ book-store/src/components/Book/Book.js
@@ -1,29 +1,38 @@
-import { Button } from '@mui/material'
-import React from 'react'
-import './Book.css'
-import {Link, useNavigate} from 'react-router-dom';
-import axios from 'axios'
+import { Button } from "@mui/material";
+import React from "react";
+import "./Book.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Book(props) {
   const history = useNavigate();
-    
-    const {_id, name, author, desctiption, price, available, image} = props.book
 
-    const deleteHandler = async() => {
-    await  axios.delete(`http://localhost:5000/books/${_id}`).then((res) => res.data).then(() => history('/') ).then(() => history('/books') )
-    }
+  const { _id, name, author, desctiption, price, available, image } =
+    props.book;
+
+  const deleteHandler = async () => {
+    await axios
+      .delete(`http://localhost:5000/books/${_id}`)
+      .then((res) => res.data)
+      .then(() => history("/"))
+      .then(() => history("/books"));
+  };
 
   return (
-    <div className='card'>
+    <div className="card">
       <img src={image} alt="" />
       <article> By {author} </article>
       <h3>{name}</h3>
       <p>{desctiption}</p>
       <h2>{price}</h2>
-      <Button LinkComponent={Link} to={`/books/${_id}`} >Update</Button>
-      <Button onClick={deleteHandler}>Delete</Button>
+      <div className="buttons">
+        <Button LinkComponent={Link} to={`/books/${_id}`}>
+          Update
+        </Button>
+        <Button onClick={deleteHandler}>Delete</Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Book
+export default Book;
